09a3ebe701d48548e9643ad510cfa216e4422ac2
Lierod <dujiadi@gmail.com>
2019-04-22 13:31:33 -0500

    neighbour area graph
    neighbour area graph

---
+++ jiadi/graph.py
@@ -0,0 +1,50 @@
+import pickle
+from geopy.distance import geodesic
+from collections import defaultdict
+# from numba import jit
+
+
+def save_obj(obj, name ):
+    with open(name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL)
+
+
+def load_obj(name ):
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+
+
+# @jit
+
+sell = load_obj("sell_data")
+print(list(sell))
+coordinates = []
+with open("newhouse_basic_formated.txt", 'r', encoding='utf-8') as f:
+    line = f.readline()[1:]
+    # print(line[:-1], len(line))
+    # cnt = 1
+    while line:
+        if line[0:3] == 'bid':
+            index_ori = int(line.split()[1])
+            line = f.readline()
+            lat = float(line.split()[1])
+            line = f.readline()
+            lng = float(line.split()[1])
+            # print(lat,lng)
+            if index_ori in list(sell):
+                index_new = list(sell).index(index_ori)
+                # print(index_ori, index_new)
+                coordinates.insert(index_new, {'index_ori': index_ori, 'lat': lat, 'lng': lng})
+        line = f.readline()
+        
+graph = defaultdict(list)
+for i in range(coordinates.__len__() - 1):
+    print(i)
+    for j in range(i + 1, coordinates.__len__()):
+        location1 = (coordinates[i]['lat'], coordinates[i]['lng'])
+        location2 = (coordinates[j]['lat'], coordinates[j]['lng'])
+        dis = geodesic(location1, location2).kilometers
+        if dis <= 8:
+            graph[i].append(j)
+            graph[j].append(i)
+save_obj(graph, "graph_8km")
---
+++ jiadi/test.py
@@ -0,0 +1,18 @@
+# from geopy.distance import geodesic
+# print(geodesic((39.907655, 116.404194),(39.908806, 116.417778)).kilometers)
+
+
+# from collections import defaultdict
+# s = [('yellow', 1), ('blue', 2), ('yellow', 3), ('blue', 4), ('red', 1)]
+# d = defaultdict(list)
+# for k, v in s:
+#     d[k].append(v)
+#
+# sorted(d.items())
+
+import pickle
+def load_obj(name ):
+    with open(name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+graph = load_obj("graph_500m")
+print(graph)
